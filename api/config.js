654dde67e8af0e8cf7c3fcf5c307ac2eef730488
Lavensoft <46833976+lavensoft@users.noreply.github.com>
2023-05-18 03:47:59 +0700

    U

--- api/config.js
+++ api/config.js
@@ -3,7 +3,7 @@ import CONFIG from '../global/config';
 const API_KEY = 'lavenesnewsapikeyghdnjmkljd@1239980004!'
 
 const config = {
-    API_URL: CONFIG.DEBUG ? 'http://192.168.1.3:8082/api/v1' : 'https://server.news.lavenes.com:8082/api/v1',
+    API_URL: CONFIG.DEBUG ? 'http://192.168.1.3:8082/api/v1' : 'https://api.news.lavenes.com/api/v1',
     API_KEY: API_KEY,
     HEADERS: {
         "api_key": API_KEY
